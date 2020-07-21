d941820265eada5408394debcaa2962e78f96cb0
Katie Le <lethikatie@gmail.com>
2020-07-21 21:32:19 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,18 +1,4 @@
-class Cat
+class Cat 
+  def 
   
-  def name=(cats_name)
-    @name = "Maru"
-  end 
-  
-  attr_accessor :name
- 
- def meow
-   @meow
-   puts "meow!"
-  end
 end 
-
-maru = Cat.new
-maru.name = "Maru"
-
-puts maru.name
