26c7539fa2ca5e65fea1ac235cc482098980c3ba
Katie Le <lethikatie@gmail.com>
2020-07-21 21:35:23 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,18 +1,11 @@
-class Cat
-  
-  def name=(cats_name)
-    @name = "Maru"
-  end 
-  
-  attr_accessor :name
+class Cat 
+ def name=(cats_name)
+   @name = "Maru"
+ end 
+ 
+ attr_accessor :name
  
  def meow
-   @meow
    puts "meow!"
-  end
-end 
-
-maru = Cat.new
-maru.name = "Maru"
-
-puts maru.name
+  end 
+ end 
